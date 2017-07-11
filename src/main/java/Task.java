3c24bbb12644258de4f93c0088cac8cd800b5988
Anna Timofeeva and Joel Bakken <anna@>
2017-07-11 09:20:20 -0700

    add the ability to delete a task from the lists

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -85,5 +85,14 @@ public class Task {
         .addParameter("id", id)
         .executeUpdate();
     }
-}
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+    String sql = "DELETE FROM tasks WHERE id = :id;";
+    con.createQuery(sql)
+      .addParameter("id", id)
+      .executeUpdate();
+    }
+  }
 }
